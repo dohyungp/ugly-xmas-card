dee550ff474709770b390fb6edd9ed8a52311ab3
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2019-12-23 13:58:19 +0900

    Add Background Colours

---
+++ src/theme/purple.jsx
@@ -0,0 +1,9 @@
+export default {
+  colors: {
+    background: "#78025d",
+    cardBackground: "#E5E9F0",
+    buttonBackground: "#f5d201",
+    textColor: "#030363",
+    buttonColor: "#030363"
+  }
+};
