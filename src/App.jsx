06fa020ed3cf8b13bac2e55b7c6ddd0297bcf5b1
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2019-12-23 20:50:06 +0900

    Add Router to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ import { ThemeProvider } from "styled-components";
 import Container from "./component/Container";
 import useStores from "./useStores";
 import StartPage from "./page/StartPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function useColorTheme() {
   const { rootStore } = useStores();
   const { colorStore } = rootStore;
 
   return useObserver(() => ({
-    colorTheme: colorStore.getTheme,
-    goButtonSVG: colorStore.getGoButtonSVG
+    colorTheme: colorStore.getTheme
   }));
 }
 
 function App() {
-  const { colorTheme, goButtonSVG } = useColorTheme();
+  const { colorTheme } = useColorTheme();
   return (
     <ThemeProvider theme={colorTheme}>
       <Container>
-        <StartPage goButtonSVG={goButtonSVG} />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={StartPage} />
+          </Switch>
+        </Router>
       </Container>
     </ThemeProvider>
   );
--- src/page/StartPage.jsx
+++ src/page/StartPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import useStores from "../useStores";
+import { useObserver } from "mobx-react";
 import StartButton from "../component/StartButton";
 import StartBackground from "../component/StartBackground";
 import CatTV from "../component/CatTV";
 
-function StartPage({ goButtonSVG }) {
+function useColorTheme() {
+  const { rootStore } = useStores();
+  const { colorStore } = rootStore;
+
+  return useObserver(() => ({
+    goButtonSVG: colorStore.getGoButtonSVG
+  }));
+}
+
+function StartPage() {
+  const { goButtonSVG } = useColorTheme();
   return (
     <>
       <StartButton src={goButtonSVG} alt="GO" />
