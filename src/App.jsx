0d44a54a7fbe08113921106d16012c82511fd924
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2019-12-23 15:06:42 +0900

    Remove App.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useObserver } from "mobx-react";
 import { ThemeProvider } from "styled-components";
 import Container from "./component/Container";
 import useStores from "./useStores";
-import "./App.css";
 
 function useColorTheme() {
   const { rootStore } = useStores();
