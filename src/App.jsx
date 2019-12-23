77058a230fd6d6c496b97ba9a99d1a2fd4fa2d95
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2019-12-23 14:52:29 +0900

    Add Color Mobx Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useObserver } from "mobx-react";
 import { ThemeProvider } from "styled-components";
-import "./App.css";
 import Container from "./component/Container";
-import { getTheme } from "./utils/functions";
+import useStores from "./useStores";
+import "./App.css";
+
+function useColorTheme() {
+  const { rootStore } = useStores();
+  const { colorStore } = rootStore;
+
+  return useObserver(() => ({
+    colorTheme: colorStore.getTheme()
+  }));
+}
 
 function App() {
+  const { colorTheme } = useColorTheme();
   return (
-    <ThemeProvider theme={getTheme(0)}>
+    <ThemeProvider theme={colorTheme}>
       <Container></Container>
     </ThemeProvider>
   );
