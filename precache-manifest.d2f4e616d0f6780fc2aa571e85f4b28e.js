b7b8124542432dad6c7da2a8ca7b1d08c7a77754
Dohyung Park <dohyung@Dohyungui-MacBookPro.local>
2019-12-24 16:38:38 +0900

    Updates

---
+++ precache-manifest.d2f4e616d0f6780fc2aa571e85f4b28e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7873fa6600eb5c34bfcbe260ff4611b0",
+    "url": "/ugly-xmas-card/index.html"
+  },
+  {
+    "revision": "393ce53e8561379aa320",
+    "url": "/ugly-xmas-card/static/css/main.c802caec.chunk.css"
+  },
+  {
+    "revision": "4e2397b6ded529c23511",
+    "url": "/ugly-xmas-card/static/js/2.1c249e71.chunk.js"
+  },
+  {
+    "revision": "fce4dd5e4b7dbb374044cc5edf8fe918",
+    "url": "/ugly-xmas-card/static/js/2.1c249e71.chunk.js.LICENSE"
+  },
+  {
+    "revision": "393ce53e8561379aa320",
+    "url": "/ugly-xmas-card/static/js/main.2f0fb3d1.chunk.js"
+  },
+  {
+    "revision": "2bd2cd4f4d9daba3a1ef",
+    "url": "/ugly-xmas-card/static/js/runtime-main.880d8205.js"
+  },
+  {
+    "revision": "c45f0e78d53495988f92da47cd599603",
+    "url": "/ugly-xmas-card/static/media/black-theme-go-button.c45f0e78.svg"
+  },
+  {
+    "revision": "97f9fdbe1701a633c3fd0ce1301ea3ea",
+    "url": "/ugly-xmas-card/static/media/blue-rudolph.97f9fdbe.svg"
+  },
+  {
+    "revision": "6d50bd03d5210bac204469779ae6c0de",
+    "url": "/ugly-xmas-card/static/media/cake-button.6d50bd03.svg"
+  },
+  {
+    "revision": "8f6ad322962a69bcff1c7c39c7a6fe57",
+    "url": "/ugly-xmas-card/static/media/cat-face-button.8f6ad322.svg"
+  },
+  {
+    "revision": "82be94e75bb06d750bd07604caedce3a",
+    "url": "/ugly-xmas-card/static/media/christmas-ribon.82be94e7.svg"
+  },
+  {
+    "revision": "ee511fba906da774e8471c9cae0357f7",
+    "url": "/ugly-xmas-card/static/media/create-background.ee511fba.png"
+  },
+  {
+    "revision": "0344c4216f469b110ad63aca11662146",
+    "url": "/ugly-xmas-card/static/media/green-theme-go-button.0344c421.svg"
+  },
+  {
+    "revision": "b595269b815fe363e6abf14bc7115b61",
+    "url": "/ugly-xmas-card/static/media/left-cat-tv.b595269b.png"
+  },
+  {
+    "revision": "f968172c7f4b510e8c8eca3e131f7605",
+    "url": "/ugly-xmas-card/static/media/lgreen-angola.f968172c.svg"
+  },
+  {
+    "revision": "056d6cd9c763ee324f37b8d8c174c57f",
+    "url": "/ugly-xmas-card/static/media/lgreen-rudolph.056d6cd9.svg"
+  },
+  {
+    "revision": "a5fc76a8d37130b071c48d0b6fc7e4eb",
+    "url": "/ugly-xmas-card/static/media/red-angola.a5fc76a8.svg"
+  },
+  {
+    "revision": "3c85edfae44f2d556695d39709c50c53",
+    "url": "/ugly-xmas-card/static/media/red-banner.3c85edfa.svg"
+  },
+  {
+    "revision": "5820e9b9b01f2c5f4b8e406573a564d2",
+    "url": "/ugly-xmas-card/static/media/right-cat-tv.5820e9b9.png"
+  },
+  {
+    "revision": "80c9c547da2757c3b8df8024a68be977",
+    "url": "/ugly-xmas-card/static/media/sky-angola.80c9c547.svg"
+  },
+  {
+    "revision": "ca5e0b3c928d5dbddfc742c642134aca",
+    "url": "/ugly-xmas-card/static/media/sky-rudolph.ca5e0b3c.svg"
+  },
+  {
+    "revision": "559e6bc3d369adeba18f903d84cae65c",
+    "url": "/ugly-xmas-card/static/media/start-background.559e6bc3.png"
+  },
+  {
+    "revision": "c96b37e5c3949cdbd7edefbbe636b0b0",
+    "url": "/ugly-xmas-card/static/media/yellow-angola.c96b37e5.svg"
+  },
+  {
+    "revision": "0881fec2eef005af23ccc8a8a6e0c3c1",
+    "url": "/ugly-xmas-card/static/media/yellow-rudolph.0881fec2.svg"
+  },
+  {
+    "revision": "c4d7a26afe8d4e6cad3e06f2b2100e2b",
+    "url": "/ugly-xmas-card/static/media/yellow-theme-go-button.c4d7a26a.svg"
+  }
+]);
